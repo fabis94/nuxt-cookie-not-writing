4de0bb5bf75a5f4c22389b2678d8eb9c43941a9f
Kristaps Fabians Geikins <fabis94@live.com>
2024-04-09 15:46:52 +0300

    cookie default value not set in CSR

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup lang="ts">
+
+const defineCookie = (name: string) => {
+  useCookie(name, {
+    default: () => 'test-value',
+    maxAge: 60 * 60 * 24 * 7,
+  })
+}
+
+if (process.client) {
+  defineCookie('xx-test-csr-cookie')
+}
+defineCookie('xx-test-universal-cookie')
+
+</script>
